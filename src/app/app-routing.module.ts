9b8139403cb6d19b4a2b0af7f36ee5bd1f1faf58
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-05-04 13:03:07 -0400

    Creados módulos de prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
+  { path: 'test', loadChildren: () => import('./test/test.module').then(m => m.TestModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
